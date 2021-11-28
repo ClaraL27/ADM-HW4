a182d5c2c1ef12ae52b2df8a1ce633796748b8cd
edlor <56137280+edlor@users.noreply.github.com>
2021-11-28 01:08:06 +0100

    Silhouette analysis plot

--- kmeans.py
+++ kmeans.py
@@ -5,9 +5,10 @@ import seaborn as sns
 
 sns.set()
 
-#TODO
-# plot silhouette analysis
+CLUSTER_PALETTE = "muted"
 
+# TODO 
+# xlabel, ylabel in model.display
 
 def eucl_sq_dist(point1, point2):
     """
@@ -108,7 +109,61 @@ class KMeans:
                                
         return inertia
     
-    def compute_silhouette(self, data):
+    
+    def mean_dist_same_cluster(self, cluster_points, point, idx):
+        """
+        Computes the mean distance of a point to
+        the points belonging to the same cluster
+        
+        Arguments
+            cluster_points : pd dataframe with the points
+                             belonging to the same cluster as 'point'
+            point : (numpy array)
+            idx   : (int) index of point within 'cluster_points'
+            
+        Returns
+            (float) the mean distance
+                                
+        """
+        
+        a = np.sum([np.sqrt(eucl_sq_dist(point, cp)) 
+                   for cp in cluster_points.drop(idx).values])
+        
+        return a / (len(cluster_points) - 1)
+    
+
+    def mean_dists_other_clusters(self, data, point, idx):
+        """
+        Computes the mean distances of a point to
+        the points belonging to the other clusters
+        
+        Arguments
+            data  : pd dataframe with all points
+            point : (numpy array)
+            idx   : (int) index of point within 'data'
+            
+        Returns
+            (numpy array) mean distances of 'point' for each cluster
+                                
+        """
+
+        b = []   
+        
+        # iterates over the labels of the other clusters
+        # (those that are NOT the one 'point' belongs to)
+        for label in filter(lambda k: k != self.labels[idx], range(self.K)):
+                    
+            other_points = data.iloc[self.labels == label]
+            
+            b_k = np.sum([np.sqrt(eucl_sq_dist(point, op))
+                             for op in other_points.values])
+            
+            b.append(b_k / len(other_points))
+            
+        return b
+
+
+    def silhouette(self, data, plot = False):
         """
         Computes the silhouette score for each point,
         defined as
@@ -120,9 +175,6 @@ class KMeans:
         and b is the minimum distance among 
         the mean distances between a point 
         and all points in another cluster
-        
-        Finally, it returns the average score
-        over all points
         """
         
         if self.K == 1:
@@ -141,32 +193,70 @@ class KMeans:
                 s[idx] = 0
                 continue
             
-            # mean distance between 'point' and all the others
-            # in the same cluster
-            a = np.sum([np.sqrt(eucl_sq_dist(point, cp)) 
-                       for cp in cluster_points.drop(idx).values])
-            
-            a = a / (len(cluster_points) - 1)
-            
-            # mean distances of 'point' to the points
-            # of each other cluster
-            b = []            
-            for label in filter(lambda k: k != self.labels[idx], range(self.K)):
-            
-                other_points = data.iloc[self.labels == label]
-                
-                b_k = np.sum([np.sqrt(eucl_sq_dist(point, op))
-                                 for op in other_points.values])
-                
-                b.append(b_k / len(other_points))
-                
-            b_min = np.min(b)
-            
-            # silhouette score for 'point' 
+            a = self.mean_dist_same_cluster(cluster_points, point, idx)
+            b_min = np.min(self.mean_dists_other_clusters(data, point, idx))
+
             s[idx] = (b_min - a) / (np.max([a, b_min]))
             
+        if plot:
+            self.plot_silhouette(data, s)
 
-        return np.mean(s)
+        return s
+    
+    
+    def plot_silhouette(self, data, silh):
+        """
+        Plots the silhouette analysis for 
+        a certain model, in addition to a scatterplot
+        with the clusters obtained
+        """
+        
+        sns.set_style("dark")
+        
+        fig = plt.figure(figsize = (15, 5))
+        
+        gs = fig.add_gridspec(1, self.K, hspace = 0, wspace = 0)
+        axs = gs.subplots(sharex = 'col', sharey = 'row')
+        
+        fig.suptitle(f"Silhouette analysis for all {self.K} clusters")
+        axs[0].set_ylabel("Silhouette scores")
+        
+        for k in range(self.K):
+        
+            # filters data so it only includes points
+            # in cluster 'k'
+            # and adds a column with the silhouette score
+            # for the points in that cluster 'k'
+            cluster = data.iloc[self.labels == k].copy()
+            cluster["s"] = silh[self.labels == k]
+            
+            # sorts the values in 'cluster' by their
+            # silhouette score
+            cluster.sort_values(by = "s", 
+                                ascending = False, 
+                                inplace = True,
+                                ignore_index = True)
+            
+            axs[k].bar(cluster.index, cluster["s"],
+                       edgecolor = "None", 
+                       width = 1, 
+                       color = sns.color_palette(CLUSTER_PALETTE)[k])
+            
+            # adds a horizontal line with the average score
+            # over ALL points (not just over cluster 'k')
+            axs[k].hlines(y = np.mean(silh), 
+                          xmin = 0, xmax = cluster.index[-1], 
+                          color = "black",
+                          linestyle = "dashed")
+            
+            axs[k].set_xticks([])
+            axs[k].set_xlabel(k)
+        
+        plt.show()
+        
+        # scatterplot with the clusters
+        self.display(data.iloc[:,0], data.iloc[:,1],
+             show_centroids = True)
     
 
     def fit(self, data):
@@ -197,7 +287,13 @@ class KMeans:
         obtained
         """
         
-        plt.scatter(x, y, c = self.labels, edgecolors = "b")
+        sns.set_style("darkgrid")
+        
+        plt.scatter(x, y, 
+                    edgecolors = "white", 
+                    alpha = 0.8,
+                    color = [sns.color_palette(CLUSTER_PALETTE)[label]
+                             for label in self.labels])
         
         if show_centroids:
             
@@ -205,9 +301,9 @@ class KMeans:
             y_c = np.transpose(self.centroids)[1]
             
             plt.scatter(x_c, y_c, 
-                        color = "green", 
-                        marker = "X", s = 80,
-                        edgecolors = "w")
+                        color = "black",
+                        edgecolors = "white",
+                        marker = "X", s = 80)
         
         plt.show()
 
@@ -237,6 +333,8 @@ def plot_elbow(K_list, inertia):
     for different number of clusters
     """
     
+    sns.set_style("darkgrid")
+    
     plt.title("KMeans SSE (Inertia)")
     plt.xlabel("Number of clusters")
     plt.ylabel("Inertia")
@@ -248,9 +346,11 @@ def plot_elbow(K_list, inertia):
     plt.vlines(x = K_list, 
                ymin = [0]*len(K_list), ymax = inertia,
                linestyles = "dotted")
+    
+    plt.show()
 
 
-def silhouette(data, K_list, plot = False):
+def avg_silhouette(data, K_list, plot = False):
     """
     Runs KMeans for different numbers of clusters
     and computes the inertia (SSE) for each
@@ -261,19 +361,21 @@ def silhouette(data, K_list, plot = False):
     for k in K_list:
         kmeans = KMeans(k)
         kmeans.fit(data)
-        silh.append(kmeans.compute_silhouette(data))
+        silh.append(np.mean(kmeans.silhouette(data)))
         
     if plot:
-        plot_silhouette(K_list, silh)
+        plot_avg_silhouette(K_list, silh)
         
     return silh
 
 
-def plot_silhouette(K_list, silh):
+def plot_avg_silhouette(K_list, silh):
     """
     Creates a plot of the inertias obtained
     for different number of clusters
     """
+    
+    sns.set_style("darkgrid")
     
     plt.title("Average Silhouette")
     plt.xlabel("Number of clusters")
@@ -283,6 +385,4 @@ def plot_silhouette(K_list, silh):
     
     plt.xticks(ticks = K_list, labels = K_list)
     
-    # plt.vlines(x = K_list, 
-    #            ymin = [0]*len(K_list), ymax = silh,
-    #            linestyles = "dotted")
+    plt.show()
